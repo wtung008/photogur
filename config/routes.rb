50e1f57c76f8430a13e3ad1946c66b4b0a416fe5
wtung008 <wayne.h.tung@gmail.com>
2017-01-29 23:28:49 -0500

    Adding feature to create new picture using HTML form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'pictures' => 'pictures#index'
+
+  post 'pictures' => 'pictures#create'
+  get 'pictures/new' => 'pictures#new'
+  
   get 'pictures/:id' => 'pictures#show', as: 'picture'
 
 end
